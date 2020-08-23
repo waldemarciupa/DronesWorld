6fd0376a24fa6e55c65e0d33295f7352677241c4
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-23 12:19:29 +0200

    starting express

--- src/server.js
+++ src/server.js
@@ -6,3 +6,11 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello from express')
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
