5e691516cd797306ff0d9012afe117cbaa55459d
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-23 18:46:26 +0200

    worked on creating user in databasse

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const UserController = require('./controllers/UserController');
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -13,8 +14,10 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello from nodemon')
+    res.send('Hello from nodemon');
 })
+
+app.post('/register', UserController.store)
 
 try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION,
