55e9d9dd43bb2a482dc3210061462e10709913aa
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-25 20:37:52 +0200

    added option argument to eventschema and create virtual property thumbnail_url

--- src/models/Event.js
+++ src/models/Event.js
@@ -10,6 +10,14 @@ const EventSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
+}, {
+    toJSON: {
+        virtuals: true
+    }
+})
+
+EventSchema.virtual("thumbnail_url").get(function () {
+    return `http://localhost:8000/files/${this.thumbnail}`;
 })
 
 module.exports = mongoose.model('Event', EventSchema);
