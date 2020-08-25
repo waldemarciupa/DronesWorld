5d5da5e68be1b993e7a1231d5e6bc14a4df58068
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-25 06:39:40 +0200

    created eventcontroller

---
+++ src/controllers/EventController.js
@@ -0,0 +1,23 @@
+const Event = require('../models/Event');
+const User = require('../models/User');
+
+module.exports = {
+    async createEvent(req, res) {
+        const { title, description, price } = req.body;
+        const { userId } = req.headers;
+        const user_id = await User.findById(userId);
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'User does not exist!'
+            })
+        }
+
+        const event = await Event.create({
+            title,
+            description,
+            price,
+            user: user_id
+        })
+    }
+}
