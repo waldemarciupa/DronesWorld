d3297c46e4003ca65113a81b75331882aea72a58
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-28 08:41:54 +0200

    created Dashboard folder and component draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './pages/Login/index';
+import Dashboard from './pages/Dashboard/index';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Dashboard />
     </div>
   );
 }
