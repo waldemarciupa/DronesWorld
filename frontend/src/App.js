10b0394df4db0ef74c0063f4510e2e9f83af696e
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-28 08:40:20 +0200

    created Login folder and component draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login/index';
 
 function App() {
   return (
     <div className="App">
-      Learn MERN
+      <Login />
     </div>
   );
 }
