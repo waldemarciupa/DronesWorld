0989b8b23c5754ae0d68a5572934645608d51a8f
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-02 18:50:59 +0200

    added form from reactstrap

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import api from '../../services/api';
+import { Button, Form, FormGroup, Label, Input, } from 'reactstrap';
 
 export default function Login() {
     return (
-        <div>
-            Hello from Login
-        </div>
+        <Form inline>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="something@idk.cool" />
+            </FormGroup>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                <Label for="examplePassword" className="mr-sm-2">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
+            </FormGroup>
+            <Button>Submit</Button>
+        </Form>
     )
 }
