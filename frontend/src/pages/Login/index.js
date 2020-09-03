1476552bd5a5b4a13c1752ff2c59442be3e4e17c
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-03 06:51:17 +0200

    added handle submit to login form

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -6,21 +6,30 @@ export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleSubmit = async event => {
+        event.preventDefault();
+        console.log(email, password);
+    }
+
     return (
-        <Form >
+        <Form onSubmit={handleSubmit}>
             <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                 <Input
                     type="email"
                     name="email"
                     id="email"
-                    placeholder="Email" />
+                    placeholder="Email"
+                    onChange={event => setEmail(event.target.value)}
+                />
             </FormGroup>
             <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                 <Input
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="Password" />
+                    placeholder="Password"
+                    onChange={event => setPassword(event.target.value)}
+                />
             </FormGroup>
             <Button>Submit</Button>
         </Form>
