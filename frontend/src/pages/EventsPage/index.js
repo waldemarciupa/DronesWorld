26aeb90743718c705ba570f54dd6c5463131787a
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-04 06:52:25 +0200

    added states for event schema

--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Events willl show all events
+
+// title: String,
+// description: String,
+// price: Number,
+// thumbnail: String,
+// date: Date,
+// sport: String,
+
 export default function EventsPage() {
+    const user_id = localStorage.getItem('user');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [thumbnail, setThumbnail] = useState('');
+    const [date, setDate] = useState('');
+
     return (
         <div>
             Hello from Events
