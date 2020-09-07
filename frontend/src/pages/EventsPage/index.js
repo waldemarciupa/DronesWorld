174b1303415a86e4fe9eb6c75cc7e5e48a605d04
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-07 07:39:46 +0200

    added styling to create event form

--- frontend/src/pages/EventsPage/index.js
+++ frontend/src/pages/EventsPage/index.js
@@ -5,7 +5,7 @@ import CameraIcon from '../../assets/camera.png';
 import './events.css';
 
 // Events willl show all events
-export default function EventsPage() {
+export default function EventsPage({ history }) {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
@@ -56,74 +56,86 @@ export default function EventsPage() {
         <Container>
             <h1>Create your Event</h1>
             <Form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <Label>Upload Image: </Label>
-                    <Label id='thumbnail' style={{ backgroundImage: `url(${preview})` }}
-                        className={thumbnail ? 'has-thumbnail' : ''}>
+                <div className="input-group">
+                    <FormGroup>
+                        <Label>Upload Image: </Label>
+                        <Label id='thumbnail' style={{ backgroundImage: `url(${preview})` }}
+                            className={thumbnail ? 'has-thumbnail' : ''}>
+                            <Input
+                                type="file"
+                                onChange={(event) => setThumbnail(event.target.files[0])}
+                            />
+                            <img
+                                src={CameraIcon}
+                                style={{ maxWidth: "50px" }}
+                                alt="upload icon image"
+                            />
+                        </Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Sport: </Label>
                         <Input
-                            type="file"
-                            onChange={(event) => setThumbnail(event.target.files[0])}
+                            id="sport"
+                            type="text"
+                            value={sport}
+                            placeholder="Sport name"
+                            onChange={(event) => setSport(event.target.value)}
                         />
-                        <img
-                            src={CameraIcon}
-                            style={{ maxWidth: "50px" }}
-                            alt="upload icon image"
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Title: </Label>
+                        <Input
+                            id="title"
+                            type="text"
+                            value={title}
+                            placeholder="Event Title"
+                            onChange={(event) => setTitle(event.target.value)}
                         />
-                    </Label>
-                </FormGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Description: </Label>
+                        <Input
+                            id="description"
+                            type="text"
+                            value={description}
+                            placeholder="Event Description"
+                            onChange={(event) => setDescription(event.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Price: </Label>
+                        <Input
+                            id="price"
+                            type="text"
+                            value={price}
+                            placeholder="Event Price"
+                            onChange={(event) => setPrice(event.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Date: </Label>
+                        <Input
+                            id="date"
+                            type="date"
+                            value={date}
+                            placeholder="Event Date"
+                            onChange={(event) => setDate(event.target.value)}
+                        />
+                    </FormGroup>
+                </div>
                 <FormGroup>
-                    <Label>Sport: </Label>
-                    <Input
-                        id="sport"
-                        type="text"
-                        value={sport}
-                        placeholder="Sport name"
-                        onChange={(event) => setSport(event.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Title: </Label>
-                    <Input
-                        id="title"
-                        type="text"
-                        value={title}
-                        placeholder="Event Title"
-                        onChange={(event) => setTitle(event.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Description: </Label>
-                    <Input
-                        id="description"
-                        type="text"
-                        value={description}
-                        placeholder="Event Description"
-                        onChange={(event) => setDescription(event.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Price: </Label>
-                    <Input
-                        id="price"
-                        type="text"
-                        value={price}
-                        placeholder="Event Price"
-                        onChange={(event) => setPrice(event.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Date: </Label>
-                    <Input
-                        id="date"
-                        type="date"
-                        value={date}
-                        placeholder="Event Date"
-                        onChange={(event) => setDate(event.target.value)}
-                    />
-                </FormGroup>
-                <Button type="submit">
-                    Create Event
+                    <Button className="submit-btn" type="submit">
+                        Create Event
                 </Button>
+                </FormGroup>
+                <FormGroup>
+                    <Button
+                        onClick={() => history.push('/dashboard')}
+                        className="secondary-btn"
+                        type="submit">
+                        Dashboard
+                </Button>
+                </FormGroup>
             </Form>
             {errorMessage ? (
                 <Alert className="event-validation" color="danger">Missing required information</Alert>
