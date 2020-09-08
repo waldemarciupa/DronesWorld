b667a2f6d6e9cbabd197e102f47c4b1303f02a0c
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-08 07:53:02 +0200

    working on function fetching events in the dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 // Dashboard willl show all events
 export default function Dashboard() {
+    const [events, setEvents] = useState([]);
+    const user_id = localStorage.getItem('user');
+
+    const getEvents = async (filter) => {
+        const url = filter ? `/dasboard/${filter}` : '/dashboard';
+        const response = await api.get(url, { headers: { user_id } });
+
+        setEvents(response.data);
+    };
+
+    useEffect(() => {
+        getEvents();
+    }, [])
+
+    console.log(events);
     return (
         <div>
             Hello from Dashboard
