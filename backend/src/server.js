1660cfa74ec4d36af32d1f4f7445698f5d4c6bc8
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-19 16:20:16 +0200

    implementing socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-
 try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION,
         {
@@ -38,6 +37,6 @@ try {
 app.use("/files", express.static(path.resolve(__dirname, "..", "files")));
 app.use(routes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
