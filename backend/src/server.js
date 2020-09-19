ebc5a7a7493c2439d7f6580a32a1823f5a3902e9
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-19 16:51:24 +0200

    implementing socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,18 +11,6 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-const connectedUsers = {};
-
-io.on('connection', socket => {
-    const { user } = socket.handshake.query;
-
-    connectedUsers[user] = socket.id;
-    console.log(connectedUsers);
-})
-
-app.use(cors())
-app.use(express.json())
-
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
@@ -39,6 +27,23 @@ try {
     console.log(error)
 }
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+    console.log(connectedUsers);
+})
+
+// / app.use()
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+});
+app.use(cors());
+app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "files")));
 app.use(routes);
 
