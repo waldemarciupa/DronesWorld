ebb0a27859f798a2a4b3ef9d464eff2a4f1fe356
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-25 08:18:50 +0200

    added verifyToken to registration route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -22,7 +22,7 @@ routes.get('/status', (req, res) => {
 routes.post('/login', LoginController.store);
 
 // Registration
-routes.post('/registration/:eventId', RegistrationController.create);
+routes.post('/registration/:eventId', verifyToken, RegistrationController.create);
 routes.get('/registration/:registration_id', RegistrationController.getRegistration);
 routes.post('/registration/:registration_id/approvals', ApprovalController.approval);
 routes.post('/registration/:registration_id/rejections', RejectionController.rejection);
