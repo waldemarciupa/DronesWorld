76bc8911f1f3dc8002112e13b8ee1cc07ab32ac9
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-25 08:44:42 +0200

    implementing jwt into registration controller:

--- backend/src/controllers/RegistrationController.js
+++ backend/src/controllers/RegistrationController.js
@@ -1,24 +1,30 @@
-const { create } = require('../models/Registration');
 const Registration = require('../models/Registration');
+const jwt = required('jsonwebtoken');
 
 module.exports = {
     async create(req, res) {
-        const { user_id } = req.headers;
-        const { eventId } = req.params;
-        const { date } = req.body;
+        jwt.verify(req.token, 'secret', async (err, authData) => {
+            if (err) {
+                res.sendStatus(401);
+            } else {
+                const { user_id } = req.headers;
+                const { eventId } = req.params;
+                const { date } = req.body;
 
-        const registration = await Registration.create({
-            user: user_id,
-            event: eventId,
-            date
+                const registration = await Registration.create({
+                    user: user_id,
+                    event: eventId,
+                    date
+                })
+
+                await registration
+                    .populate('event')
+                    .populate('user', '-password')
+                    .execPopulate();
+
+                return res.json(registration);
+            }
         })
-
-        await registration
-            .populate('event')
-            .populate('user', '-password')
-            .execPopulate();
-
-        return res.json(registration);
     },
 
     async getRegistration(req, res) {
