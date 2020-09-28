9f0078ec722d7fa822d236f20c54b0e04cdb3afa
waldemarciupa <waldemarciupa90@gmail.com>
2020-09-28 08:12:20 +0200

    added registration handler

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -11,6 +11,7 @@ export default function Dashboard({ history }) {
     const [rSelected, setRSelected] = useState(null);
     const [error, setError] = useState(false);
     const [success, setSuccess] = useState(false);
+    const [messageHandler, setMessageHandler] = useState('');
     const user = localStorage.getItem('user');
     const user_id = localStorage.getItem('user_id');
 
@@ -46,17 +47,20 @@ export default function Dashboard({ history }) {
         try {
             const deleteEvent = await api.delete(`/event/${eventId}`, { headers: { user: user } })
             setSuccess(true);
+            setMessageHandler('The event was deleted succesfully');
             setTimeout(() => {
                 setSuccess(false);
-                handleFilter(null)
+                handleFilter(null);
+                setMessageHandler('');
             }, 2000);
 
         } catch (error) {
             setError(true);
+            setMessageHandler('Error when deleting event!');
             setTimeout(() => {
                 setError(false)
+                setMessageHandler('');
             }, 2000);
-            console.log('Missing required data');
         }
     }
 
@@ -66,12 +70,35 @@ export default function Dashboard({ history }) {
         history.push('/login');
     }
 
+    const handleRegistrationRequest = async (event) => {
+        try {
+            await api.post(`/registration/${event.id}`, {}, { headers: { user } })
+
+            setSuccess(true);
+            setMessageHandler(`The request for the event ${event.title} was succesfully`);
+            setTimeout(() => {
+                setSuccess(false);
+                handleFilter(null);
+                setMessageHandler('');
+            }, 2000);
+        } catch (error) {
+            setError(true);
+            setMessageHandler(`The request for the event ${event.title} failed`);
+            setTimeout(() => {
+                setError(false)
+                setMessageHandler('');
+            }, 2000);
+        }
+    }
+
     useEffect(() => {
         getEvents();
     }, [])
 
     useEffect(() => {
         const socket = socketio('http://localhost:8000/', { query: { user_id } })
+
+        socket.on('registration_request', data => console.log(data))
     }, [])
 
     return (
@@ -127,15 +154,15 @@ export default function Dashboard({ history }) {
                         <span>Event date: {moment(event.date).format('LL')}</span>
                         <span>Event price: {parseFloat(event.price).toFixed(2)}</span>
                         <span>Event description: {event.description}</span>
-                        <Button color="primary">Subscribe</Button>
+                        <Button color="primary" onClick={() => handleRegistrationRequest(event)}>Registration request</Button>
                     </li>
                 ))}
             </ul>
             {error ? (
-                <Alert className="event-validation" color="danger">Erro when deleting event!</Alert>
+                <Alert className="event-validation" color="danger">{messageHandler}</Alert>
             ) : ""}
             {success ? (
-                <Alert className="event-validation" color="success">The event was deleted succesfully</Alert>
+                <Alert className="event-validation" color="success">{messageHandler}</Alert>
             ) : ""}
         </>
     )
