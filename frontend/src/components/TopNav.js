b14cdf987e5ecec563ab61d25af13ddffc46a7af
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-01 08:16:18 +0200

    import Link

--- frontend/src/components/TopNav.js
+++ frontend/src/components/TopNav.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const TopNav = () => {
     const [collapsed, setCollapsed] = useState(true);
