7fa3605ebf5c58b2bca7ff78e214774dccd98dda
waldemarciupa <waldemarciupa90@gmail.com>
2020-10-02 08:23:48 +0200

    added context wrapper to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'reactstrap';
+import { ContextWrapper } from './user-context';
 import Routes from './routes';
 
 function App() {
   return (
-    <Container>
-      <h1>Sport's App</h1>
-      <div className="content">
-        <Routes />
-      </div>
-    </Container>
+    <ContextWrapper>
+      <Container>
+        <h1>Sport's App</h1>
+        <div className="content">
+          <Routes />
+        </div>
+      </Container>
+    </ContextWrapper>
   );
 }
 
